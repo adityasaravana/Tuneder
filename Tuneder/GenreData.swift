285fda06ce1d556893aa2893b2ae4f11c5eb9ab5
Aditya Saravana <aditya.saravana@icloud.com>
2023-06-21 19:23:18 -0700

    Revert "Code Cleanup & an easier way to add new genres"
    This reverts commit f424550954b612842d1bba58d59358d9211b9a19.

--- Tuneder/GenreData.swift
+++ Tuneder/GenreData.swift
@@ -9,16 +9,6 @@ import Foundation
 import MusadoraKit
 import MusicKit
 
-/// This passes data between concurrently and non-concurrently executing code, like in catalogData below
-actor GenreActor {
-    var genre: Genre? = nil
-    
-    func set(_ newValue: Genre?) {
-        genre = newValue
-    }
-}
-
-/// A simple struct that makes adding new genres to GenreSelection easier and keeps code cleaner
 struct GenreData: Codable {
     var name: String
     var id: MusicItemID?
